7e93e10450a29fe21fcc4beb8b84c06c1d12875b
lunaflight <e0958283@u.nus.edu>
2024-01-07 01:27:29 +0800

    Use mapi in put_mark

--- lib/board.ml
+++ lib/board.ml
@@ -3,11 +3,9 @@ type mark =
     | X
     | Empty;;
 
-
 type board = mark list list;;
 
 let empty_board = [[Empty; Empty; Empty]; [Empty; Empty; Empty]; [Empty; Empty; Empty]];;
-
 
 let print_board board =
 let mark_to_string mark = match mark with
@@ -17,3 +15,7 @@ let mark_to_string mark = match mark with
 let row_to_string row = String.concat " " (List.map mark_to_string row) in
 let board_to_string board = String.concat "\n" (List.map row_to_string board) in
 print_endline (board_to_string board);;
+
+let put_mark mark r c board = 
+    List.mapi (fun i x -> if i <> r then x
+    else List.mapi (fun j y -> if j <> c then y else mark) x) board;;
